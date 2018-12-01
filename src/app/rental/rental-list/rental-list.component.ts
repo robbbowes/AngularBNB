97f7e9b69e15559b7f542d18a5df7570e18becb2
Robb Bowes <up666724@myport.ac.uk>
2018-12-01 13:30:21 +0100

    Create rental class

--- src/app/rental/rental-list/rental-list.component.ts
+++ src/app/rental/rental-list/rental-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RentalService } from '../shared/rental.service';
-
+import { Rental } from '../shared/rental.model';
 @Component({
   selector: 'bnb-rental-list',
   templateUrl: './rental-list.component.html',
@@ -8,12 +8,24 @@ import { RentalService } from '../shared/rental.service';
 })
 export class RentalListComponent implements OnInit {
 
-  rentalItems: any[] = [];
+  rentalItems: Rental[] = [];
 
   constructor(private rentalService: RentalService) { }
 
   ngOnInit() {
-    this.rentalItems = this.rentalService.getRentals();
-  }
+
+    const rentalObservable = this.rentalService.getRentals();
+
+    rentalObservable.subscribe(
+      (rentalItems: Rental[]) => {
+        this.rentalItems = rentalItems;
+      },
+      (error) => {
+      },
+      () => {
+
+      }
+    )
+  };
 
 }
--- src/app/rental/shared/rental.service.ts
+++ src/app/rental/shared/rental.service.ts
@@ -1,55 +1,57 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Rental } from './rental.model';
 
 @Injectable()
 export class RentalService {
 
-    private rentals: any[] = [{
-        id: 1,
+    private rentals: Rental[] = [{
+        id: '1',
         title: 'Central Apartment',
         city: 'Berlin',
         street: 'Times Square',
         category: 'Apartment',
         image: 'http://via.placeholder.com/350x250',
-        bedrooms: '3',
+        bedrooms: 3,
         description: 'Very nice apartment',
         dailyRate: 34,
         shared: false,
         createdOn : '24/12/2018'
       },
       {
-        id: 2,
+        id: '2',
         title: 'Your new place',
         city: 'New York',
         street: 'Times Square',
         category: 'Apartment',
         image: 'http://via.placeholder.com/350x250',
-        bedrooms: '2',
+        bedrooms: 2,
         description: 'Very nice apartment',
         dailyRate: 34,
         shared: true,
         createdOn : '24/12/2014'
       },
       {
-        id: 3,
+        id: '3',
         title: 'Loops Ltd',
         city: 'New York',
         street: 'Times Square',
         category: 'Apartment',
         image: 'http://via.placeholder.com/350x250',
-        bedrooms: '3',
+        bedrooms: 3,
         description: 'Very nice apartment',
         dailyRate: 34,
         shared: false,
         createdOn : '24/12/2017'
       },
       {
-        id: 4,
+        id: '4',
         title: 'A little condo',
         city: 'New York',
         street: 'Times Square',
         category: 'Condo',
         image: 'http://via.placeholder.com/350x250',
-        bedrooms: '3',
+        bedrooms: 3,
         description: 'Great apartment',
         dailyRate: 34,
         shared: false,
@@ -57,8 +59,21 @@ export class RentalService {
       }
     ];
 
-    public getRentals(): any[] {
-        return this.rentals;
+    public getRentals(): Observable<Rental[]> {
+        const rentalObservable: Observable<Rental[]> = new Observable((observer) => {
+            setTimeout(() => {
+                observer.next(this.rentals);
+            }, 1000);
+
+            setTimeout(() => {
+                observer.error('I am an error :(');
+            }, 2000);
+
+            setTimeout(() => {
+                observer.complete();
+            }, 3000);
+        });
+        return rentalObservable;
     }
 
 }
