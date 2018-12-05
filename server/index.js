42fd203c6c708bbc2cbe5666a871bb578626288a
Robb Bowes <up666724@myport.ac.uk>
2018-12-05 11:46:12 +0100

    Abstract routes away from index.js

--- server/index.js
+++ server/index.js
@@ -2,21 +2,24 @@ const express = require('express');
 const mongoose = require('mongoose');
 const config = require('./config/dev');
 const FakeDb = require('./fake-db');
-//const Rental = require('./models/rental');
+const Rental = require('./models/rental');
 
-mongoose.connect(config.DB_URI, { useNewUrlParser: true
-}).then(() => {
-    const fakeDb = new FakeDb();
-    fakeDb.seedDb();
-});
+const rentalRoutes = require('./routes/rentals');
+
+mongoose.connect(
+    config.DB_URI,
+    {
+        useNewUrlParser: true
+    }).then(() => {
+        const fakeDb = new FakeDb();
+        fakeDb.seedDb();
+    });
 
 const app = express();
 
-app.get('/rentals', function(req, res) {
-    res.json({'success': true});
-})
+app.use('/api/v1/rentals', rentalRoutes);
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log('Running on port ' + PORT);
 });
