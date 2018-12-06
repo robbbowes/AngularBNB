b7a104ac7d6af2c43c030bcfd8c03281a516bdd3
Robb Bowes <up666724@myport.ac.uk>
2018-12-06 17:33:00 +0100

    More route stuff with jwt and error handling. Verdict: Middleware is not fun

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,40 +1,100 @@
 const User = require('../models/user');
-const { normaliseErrors } = require('../helpers/mongoose');
-exports.auth = function(req, res) {
+const { normalizeErrors } = require('../helpers/mongoose');
+const jwt = require('jsonwebtoken');
+const config = require('../config/dev');
 
+exports.auth =  function(req, res) {
+  const { email, password } = req.body;
+
+  if (!password || !email) {
+    return res.status(422).send({errors: [{title: 'Data missing!', detail: 'Provide email and password!'}]});
+  }
+
+  User.findOne({email}, function(err, user) {
+    if (err) {
+      return res.status(422).send({errors: normalizeErrors(err.errors)});
+    }
+
+    if (!user) {
+      return res.status(422).send({errors: [{title: 'Invalid User!', detail: 'User does not exist'}]});
+    }
+
+    if (user.hasSamePassword(password)) {
+      const token = jwt.sign({
+        userId: user.id,
+        username: user.username
+      }, config.SECRET, { expiresIn: '1h'});
+
+      return res.json(token);
+    } else {
+      return res.status(422).send({errors: [{title: 'Wrong Data!', detail: 'Wrong email or password'}]});
+    }
+  });
 }
 
-exports.register = function(req, res) {
-    const {username, email, password, passwordConfirmation}  = req.body;
-    
-    if (!email || !password) {
-        return res.status(422).send({error: [{title: 'Data missing!', detail: 'Email and password are mandatory'}]})
+exports.register =  function(req, res) {
+  const { username, email, password, passwordConfirmation } = req.body;
+
+  if (!password || !email) {
+    return res.status(422).send({errors: [{title: 'Data missing!', detail: 'Provide email and password!'}]});
+  }
+
+  if (password !== passwordConfirmation) {
+    return res.status(422).send({errors: [{title: 'Invalid passsword!', detail: 'Password is not a same as confirmation!'}]});
+  }
+
+  User.findOne({email}, function(err, existingUser) {
+    if (err) {
+      return res.status(422).send({errors: normalizeErrors(err.errors)});
     }
-    if (password !== passwordConfirmation) {
-        return res.status(422).send({error: [{title: 'Passwords do not match!', detail: 'Password and password confirmation do not match'}]})
+
+    if (existingUser) {
+      return res.status(422).send({errors: [{title: 'Invalid email!', detail: 'User with this email already exist!'}]});
     }
 
-    User.findOne({email}, function(err, existingUser) {
-        if (err) {
-            return res.status(422).send({errors: normaliseErrors(err.errors)})
-        }
-
-        if (existingUser) {
-            return res.status(422).send({error: [{title: 'Invalid email!', detail: 'Email belongs to another user'}]})
-        }
-
-        const user = new User({
-            username,
-            email,
-            password
-        });
-
-        user.save(function(err) {
-            if (err) {
-                return res.status(422).send({errors: normaliseErrors(err.errors)})
-            }
-
-            res.json({'registered': true})
-        })
+    const user = new User({
+      username,
+      email,
+      password
     });
+
+    user.save(function(err) {
+      if (err) {
+        return res.status(422).send({errors: normalizeErrors(err.errors)});
+      }
+
+      return res.json({'registered': true});
+    })
+  })
+}
+
+exports.authMiddleware = function(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (token) {
+    const user = parseToken(token);
+
+    User.findById(user.userId, function(err, user) {
+      if (err) {
+        return res.status(422).send({errors: normalizeErrors(err.errors)});
+      }
+
+      if (user) {
+        res.locals.user = user;
+        next();
+      } else {
+        return notAuthorized(res);
+      }
+    })
+  } else {
+    return notAuthorized(res);
+  }
+}
+
+function parseToken(token) {
+  return jwt.verify(token.split(' ')[1], config.SECRET);
+}
+
+function notAuthorized(res) {
+  return res.status(401).send({errors: [{title: 'Not authorized!', detail: 'You need to login to get access!'}]});
 }
